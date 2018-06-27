e7eb62e8b7d1891b2ddec90e7d65c862774a4496
R0quef0rt <ryanjbrooks11@gmail.com>
2018-06-27 00:53:55 -0500

    Try init

--- srv/terraform/terraform-init.sh
+++ srv/terraform/terraform-init.sh
@@ -5,17 +5,17 @@ if   [ "$SERVICE_NAME" != "buckets" ]; then
     --reconfigure \
     -input=false \
     -upgrade \
-    -backend-config=key=$PROVIDER/$CI_PROJECT_PATH/$PROJECT_KEY/terraform.tfstate \
-    -backend-config=bucket=cdi-tfstate-$ACCOUNT-$ENV-$REGION \
+    -backend-config=key=$ENV/$SERVICE_NAME/$PROJECT_KEY/terraform.tfstate \
+    -backend-config=bucket=$TFSTATE_BUCKET \
     -backend-config=region=$REGION \
-    -backend-config=dynamodb_table=$ENV-tfstate-lock
+    # -backend-config=dynamodb_table=$ENV-tfstate-lock
 elif [ "$SERVICE_NAME" = "buckets" ]; then
     terraform init \
     --reconfigure \
     -input=false \
     -upgrade \
-    -backend-config=key=$PROVIDER/$CI_PROJECT_PATH/$PROJECT_KEY/terraform.tfstate \
-    -backend-config=bucket=cdi-tfstate-$ACCOUNT-$ENV-$SERVICE_NAME \
+    -backend-config=key=$ENV/$SERVICE_NAME/$PROJECT_KEY/terraform.tfstate \
+    -backend-config=bucket=$TFSTATE_BUCKET \
     -backend-config=region=$REGION
 else
     echo "DESTROY_ALL variable is $DESTROY_ALL. Skipping destruction."	
