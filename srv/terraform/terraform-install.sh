924ef1a2b5f27f874ee44a7577b5375714ca00d9
R0quef0rt <ryanjbrooks11@gmail.com>
2018-06-26 23:48:15 -0500

    Update unzip

--- srv/terraform/terraform-install.sh
+++ srv/terraform/terraform-install.sh
@@ -2,9 +2,8 @@
 
 TERRAFORM_VERSION=0.11.7
 
-wget -sLo /tmp/terraform.zip https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-unzip /tmp/terraform.zip -d /tmp
-mkdir ~/bin
-mv /tmp/terraform ~/bin
+wget --quiet https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+mv terraform /usr/local/bin
 
-export PATH="~/bin:$PATH"
+export PATH="~/usr/local/bin:$PATH"
