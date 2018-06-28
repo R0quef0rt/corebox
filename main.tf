20ee84bfdebd4c5ac909512ee62196b7e396524f
R0quef0rt <ryanjbrooks11@gmail.com>
2018-06-27 23:24:01 -0500

    Add sudo

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_instance" "minion" {
   provisioner "salt-masterless" {
     "local_state_tree"   = "${path.root}/srv/salt"
     "local_pillar_roots" = "${path.root}/srv/pillar"
-    "disable_sudo"       = "true"
+    "disable_sudo"       = "false"
     "bootstrap_args"     = "-i cloudbox -F -P -p python-git"
     "minion_config_file" = "etc/salt/minion"
     "salt_call_args"     = "-i cloudbox"
