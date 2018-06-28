9e226626e1bf430a3155ba534acdf9686cc2193b
R0quef0rt <ryanjbrooks11@gmail.com>
2018-06-27 22:52:01 -0500

    Try again

--- main.tf
+++ main.tf
@@ -49,7 +49,6 @@ resource "aws_instance" "minion" {
   }
 
   provisioner "salt-masterless" {
-    "local_state_tree"   = "${path.root}/srv/salt"
     "bootstrap_args"     = "-i cloudbox -F -P -p python-git"
     "minion_config_file" = "etc/salt/minion"
     "salt_call_args"     = "-i cloudbox"
