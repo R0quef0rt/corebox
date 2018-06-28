14e253274815e758e0ede9b0cc38e4050bc11a31
R0quef0rt <ryanjbrooks11@gmail.com>
2018-06-27 21:59:17 -0500

    Fix destroy script

--- srv/terraform/terraform-destroy.sh
+++ srv/terraform/terraform-destroy.sh
@@ -2,15 +2,15 @@
 
 export TF_WARN_OUTPUT_ERRORS=1
 
-if   [ "$DESTROY_ALL" == true ] && [ "$SERVICE_NAME" != "buckets" ] || [ "$REBUILD" == true ] && [ "$SERVICE_NAME" != "buckets" ]; then
+if   [ "$DESTROY_ALL" == true ] || [ "$REBUILD" == true ]; then
     terraform destroy \
     -force \
-    -var-file=$CI_PROJECT_DIR/conf/$ENV.tfvars \
+    -var-file=$CI_BUILD_DIR/conf/$ENV.tfvars \
     -input=false
-elif [ "$DESTROY_ALL" == true ] && [ "$SERVICE_NAME" = "buckets" ] || [ "$REBUILD" == true ] && [ "$SERVICE_NAME" = "buckets" ]; then
+elif [ "$DESTROY_ALL" == true ] || [ "$REBUILD" == true ]; then
     terraform destroy \
     -force \
-    -var-file=$CI_PROJECT_DIR/conf/$ENV.tfvars \
+    -var-file=$CI_BUILD_DIR/conf/$ENV.tfvars \
     -input=false \
     -lock=false
 else
