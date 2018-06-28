4bd65f4897e8e46d45da3bf41ad938ccd47f4c47
R0quef0rt <ryanjbrooks11@gmail.com>
2018-06-27 23:48:42 -0500

    Remove bad variable in user-data

--- user-data.sh
+++ user-data.sh
@@ -2,9 +2,3 @@
 
 mkdir -p /app
 mkdir -p /srv/salt/states
-
-# wget -O bootstrap-salt.sh https://bootstrap.saltstack.com
-# sh bootstrap-salt.sh -P -U -F \
-#   -i devbox \
-#   -c /tmp \
-#   git v${SALT_VERSION}
