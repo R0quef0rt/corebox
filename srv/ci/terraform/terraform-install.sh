824f0bbdd4a63dfa7d6bf84ad6ee1959711a58cd
Unknown <ryanjbrooks11@gmail.com>
2018-10-16 13:56:01 -0500

    Fix TF version

--- srv/ci/terraform/terraform-install.sh
+++ srv/ci/terraform/terraform-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-TERRAFORM_VERSION=0.11.7
+# TERRAFORM_VERSION=0.11.7
 
 wget --quiet "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
 unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /tmp
