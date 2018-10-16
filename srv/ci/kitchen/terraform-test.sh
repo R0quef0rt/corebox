c4a33a069c6c0f764f212e9d0885683752a00ad6
Unknown <ryanjbrooks11@gmail.com>
2018-10-16 09:49:50 -0500

    Remove unneeded variables

--- srv/ci/kitchen/terraform-test.sh
+++ srv/ci/kitchen/terraform-test.sh
@@ -1,28 +1,28 @@
 #! /usr/bin/env bash
 
 # Unset AWS STS session environment variables
-function drop_aws_sts_session {
-  unset AWS_ACCESS_KEY_ID
-  unset AWS_DEFAULT_REGION
-  unset AWS_SECRET_ACCESS_KEY
-  unset AWS_SESSION_TOKEN
-}
+# function drop_aws_sts_session {
+#   unset AWS_ACCESS_KEY_ID
+#   unset AWS_DEFAULT_REGION
+#   unset AWS_SECRET_ACCESS_KEY
+#   unset AWS_SESSION_TOKEN
+# }
 
-# Export AWS STS session environment variables
-function export_aws_sts_session {
-  drop_aws_sts_session
-  session="$(aws sts get-session-token --output=json)"
-  AWS_ACCESS_KEY_ID="$(echo $session | jq -r .Credentials.AccessKeyId)"
-  AWS_DEFAULT_REGION="$1"
-  AWS_SECRET_ACCESS_KEY="$(echo $session | jq -r .Credentials.SecretAccessKey)"
-  AWS_SESSION_TOKEN="$(echo $session | jq -r .Credentials.SessionToken)"
-  export AWS_ACCESS_KEY_ID
-  export AWS_DEFAULT_REGION
-  export AWS_SECRET_ACCESS_KEY
-  export AWS_SESSION_TOKEN
-}
+# # Export AWS STS session environment variables
+# function export_aws_sts_session {
+#   drop_aws_sts_session
+#   session="$(aws sts get-session-token --output=json)"
+#   AWS_ACCESS_KEY_ID="$(echo $session | jq -r .Credentials.AccessKeyId)"
+#   AWS_DEFAULT_REGION="$1"
+#   AWS_SECRET_ACCESS_KEY="$(echo $session | jq -r .Credentials.SecretAccessKey)"
+#   AWS_SESSION_TOKEN="$(echo $session | jq -r .Credentials.SessionToken)"
+#   export AWS_ACCESS_KEY_ID
+#   export AWS_DEFAULT_REGION
+#   export AWS_SECRET_ACCESS_KEY
+#   export AWS_SESSION_TOKEN
+# }
 
-export_aws_sts_session "us-east-1"
+# export_aws_sts_session "us-east-1"
 
 # Destroy any existing Terraform state in us-east-1
 # bundle exec kitchen destroy centos
@@ -46,4 +46,4 @@ export_aws_sts_session "us-east-1"
 # Perform the same steps for Ubuntu in us-west-2
 bundle exec kitchen test ubuntu
 
-drop_aws_sts_session
+# drop_aws_sts_session
