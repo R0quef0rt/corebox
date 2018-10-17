4a3bef357b63960dc854ee8b42ca12f9053a38a3
Unknown <ryanjbrooks11@gmail.com>
2018-10-17 16:44:18 -0500

    Attempt VPC creation

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ provider "random" {
 
 terraform {
   required_version = ">= 0.11.4, < 0.12.0"
+
   backend "s3" {
     encrypt = "true"
   }
@@ -32,9 +33,9 @@ data "aws_ami" "minion" {
 }
 
 resource "aws_security_group" "minion" {
-  name        = "${var.service_name}-${var.env}-minion-${random_string.name.result}"
+  name        = "${var.service_name}-${var.env}-minion-${random_string.main.result}"
   description = "Used by the AWS instance."
-  vpc_id      = "vpc-cd2d97b4"
+  vpc_id      = "${module.vpc.vpc_id}"
 
   ingress {
     from_port = 22
@@ -57,7 +58,7 @@ resource "aws_instance" "minion" {
   ami           = "${data.aws_ami.minion.image_id}"
 
   key_name               = "${aws_key_pair.main.key_name}"
-  subnet_id              = "subnet-df80f097"
+  subnet_id              = "${element(module.vpc.public_subnets, count.index)}"
   vpc_security_group_ids = ["${aws_security_group.minion.id}"]
 
   connection {
@@ -66,19 +67,18 @@ resource "aws_instance" "minion" {
     private_key = "${file("${var.private_key}")}"
   }
 
-# provisioner "salt-masterless" {
-#     "local_state_tree"   = "${path.root}${var.env == "test" ? "/../../.." : ""}/srv/salt"
-#     "minion_config_file" = "${path.root}${var.env == "test" ? "/../../.." : ""}/etc/salt/minion.${var.os_family}"
-#     "bootstrap_args"     = "-i cloudbox -U -F -P -p python-git"
-#     "salt_call_args"     = "--id cloudbox saltenv=${var.env} pillarenv=${var.env}"
-# }
+  # provisioner "salt-masterless" {
+  #     "local_state_tree"   = "${path.root}${var.env == "test" ? "/../../.." : ""}/srv/salt"
+  #     "minion_config_file" = "${path.root}${var.env == "test" ? "/../../.." : ""}/etc/salt/minion.${var.os_family}"
+  #     "bootstrap_args"     = "-i cloudbox -U -F -P -p python-git"
+  #     "salt_call_args"     = "--id cloudbox saltenv=${var.env} pillarenv=${var.env}"
+  # }
 
   provisioner "remote-exec" {
     inline = [
       "sudo salt-call --local --id cloudbox state.highstate saltenv=${var.env} pillarenv=${var.env} TEST=${var.salt_test}",
     ]
   }
-
   tags {
     Name        = "${var.project_key}-${var.service_name}-${var.env}"
     environment = "${var.env}"
@@ -87,17 +87,38 @@ resource "aws_instance" "minion" {
 }
 
 resource "aws_key_pair" "main" {
-  key_name = "${var.env}-${var.service_name}-${random_string.key_name.result}"
+  key_name = "${var.env}-${var.service_name}-${random_string.main.result}"
 
   public_key = "${file("${var.public_key}")}"
 }
 
-resource "random_string" "key_name" {
+resource "random_string" "main" {
   length  = 9
   special = false
 }
 
-resource "random_string" "name" {
-  length  = 9
-  special = false
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "${var.service_name}-${var.env}-${random_string.main.result}"
+  cidr = "${var.cidr_block}"
+
+  azs             = ["${lookup(var.subnetaz1, var.region)}", "${lookup(var.subnetaz2, var.region)}", "${lookup(var.subnetaz3, var.region)}"]
+  private_subnets = ["${cidrsubnet(var.cidr_block, 3, 1)}", "${cidrsubnet(var.cidr_block, 3, 2)}", "${cidrsubnet(var.cidr_block, 3, 3)}"]
+  public_subnets  = ["${cidrsubnet(var.cidr_block, 4, 8)}", "${cidrsubnet(var.cidr_block, 4, 9)}", "${cidrsubnet(var.cidr_block, 4, 10)}"]
+
+  create_vpc = true
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+
+  enable_vpn_gateway   = false
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "${var.project_key}-${var.service_name}-${var.env}"
+    environment = "${var.env}"
+    Terraform   = "true"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -33,6 +33,54 @@ variable "region" {
   default     = "us-east-1"
 }
 
+variable "subnetaz1" {
+  description = "The availability zone to use for public subnet A."
+  type        = "map"
+
+  default = {
+    us-east-1      = "us-east-1a"
+    us-east-2      = "us-east-2a"
+    us-west-1      = "us-west-1a"
+    us-west-2      = "us-west-2a"
+    eu-west-1      = "eu-west-1a"
+    eu-west-2      = "eu-west-2a"
+    eu-central-1   = "eu-central-1a"
+    ap-southeast-1 = "ap-southeast-1a"
+  }
+}
+
+variable "subnetaz2" {
+  description = "The availability zone to use for public subnet B."
+  type        = "map"
+
+  default = {
+    us-east-1      = "us-east-1b"
+    us-east-2      = "us-east-2b"
+    us-west-1      = "us-west-1b"
+    us-west-2      = "us-west-2b"
+    eu-west-1      = "eu-west-1b"
+    eu-west-2      = "eu-west-2b"
+    eu-central-1   = "eu-central-1b"
+    ap-southeast-1 = "ap-southeast-1b"
+  }
+}
+
+variable "subnetaz3" {
+  description = "The availability zone to use for public subnet C."
+  type        = "map"
+
+  default = {
+    us-east-1      = "us-east-1c"
+    us-east-2      = "us-east-2c"
+    us-west-1      = "us-west-1c"
+    us-west-2      = "us-west-2c"
+    eu-west-1      = "eu-west-1c"
+    eu-west-2      = "eu-west-2c"
+    eu-central-1   = "eu-central-1c"
+    ap-southeast-1 = "ap-southeast-1c"
+  }
+}
+
 variable "salt_version" {
   description = "The version of Salt to install"
   default     = "2018.3.2"
@@ -66,4 +114,9 @@ variable "private_key" {
 variable "rootpath" {
   description = "Path to the terraform module"
   default     = "/"
+}
+
+variable "cidr_block" {
+  description = "The CIDR range to use for the VPC"
+  default     = "10.0.0.0/16"
 }
