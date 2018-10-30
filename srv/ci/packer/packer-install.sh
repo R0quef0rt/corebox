57cd63523700186558a2ab413b13244e55317829
roquefort <ryanjbrooks11@gmail.com>
2018-10-30 18:03:12 -0500

    Final try before merge into upstream

--- srv/ci/packer/packer-install.sh
+++ srv/ci/packer/packer-install.sh
@@ -2,5 +2,5 @@
 
 wget --quiet "https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip"
 unzip packer_${PACKER_VERSION}_linux_amd64.zip -d /tmp
-mkdir ~/bin || true
-mv /tmp/packer ~/bin
+mkdir /usr/local/bin || true
+mv /tmp/packer /usr/local/bin
--- srv/ci/terraform/terraform-install.sh
+++ srv/ci/terraform/terraform-install.sh
@@ -4,5 +4,3 @@ wget --quiet "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terr
 unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /tmp
 mkdir /usr/local/bin || true
 mv /tmp/terraform /usr/local/bin
-
-# export PATH="/usr/local/bin:$PATH"
