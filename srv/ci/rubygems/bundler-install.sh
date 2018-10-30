46518fde2bece9679461b4fc1109cc59c43cf9c6
roquefort <ryanjbrooks11@gmail.com>
2018-10-30 14:57:01 -0500

    Debug bundler

--- srv/ci/rubygems/bundler-install.sh
+++ srv/ci/rubygems/bundler-install.sh
@@ -7,3 +7,8 @@ bundle install
 # bundle show kitchen-vagrant
 # bundle show kitchen-terraform
 # bundle show test-kitchen
+
+$ rm .bundle/config # remove a broken bundler configuration
+$ gem update --system # might help if rubygems is outdated / broken
+$ gem update bundler # might help if bundler is outdated / broken
+$ bundle install # dependency hell
