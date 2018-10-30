99763fd9f0cd1529ca5ebb95a8cfb001c5002d3c
roquefort <ryanjbrooks11@gmail.com>
2018-10-30 18:11:54 -0500

    Fix mv

--- srv/ci/packer/packer-install.sh
+++ srv/ci/packer/packer-install.sh
@@ -3,4 +3,4 @@
 wget --quiet "https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip"
 unzip packer_${PACKER_VERSION}_linux_amd64.zip -d /tmp
 mkdir /usr/local/bin || true
-mv /tmp/packer /usr/local/bin
+sudo mv /tmp/packer /usr/local/bin
--- srv/ci/terraform/terraform-install.sh
+++ srv/ci/terraform/terraform-install.sh
@@ -3,4 +3,4 @@
 wget --quiet "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
 unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /tmp
 mkdir /usr/local/bin || true
-mv /tmp/terraform /usr/local/bin
+sudo mv /tmp/terraform /usr/local/bin
