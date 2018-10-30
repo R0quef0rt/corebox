1ead1efe62ca158ff37dcf293f60571b2daf95d4
roquefort <ryanjbrooks11@gmail.com>
2018-10-30 14:51:44 -0500

    Don't install bundler as root

--- srv/ci/rubygems/bundler-install.sh
+++ srv/ci/rubygems/bundler-install.sh
@@ -3,9 +3,7 @@
 apt-get -y install bundler jq
 bundle install
 
-export PATH="~/bin:$PATH"
-
-bundle show kitchen-inspec
-bundle show kitchen-vagrant
-bundle show kitchen-terraform
-bundle show test-kitchen
+# bundle show kitchen-inspec
+# bundle show kitchen-vagrant
+# bundle show kitchen-terraform
+# bundle show test-kitchen
