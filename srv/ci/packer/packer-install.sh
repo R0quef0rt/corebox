cc9860bd32b20838964504471396c68353a5e82a
roquefort <ryanjbrooks11@gmail.com>
2018-10-30 18:55:31 -0500

    Remove sudo

--- srv/ci/packer/packer-install.sh
+++ srv/ci/packer/packer-install.sh
@@ -3,4 +3,4 @@
 wget --quiet "https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip"
 unzip packer_${PACKER_VERSION}_linux_amd64.zip -d /tmp
 mkdir /usr/local/bin || true
-sudo mv /tmp/packer /usr/local/bin
+mv /tmp/packer /usr/local/bin
--- srv/ci/terraform/terraform-install.sh
+++ srv/ci/terraform/terraform-install.sh
@@ -3,4 +3,4 @@
 wget --quiet "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
 unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /tmp
 mkdir /usr/local/bin || true
-sudo mv /tmp/terraform /usr/local/bin
+mv /tmp/terraform /usr/local/bin
